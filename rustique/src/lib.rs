56029db44623d5bba53d20ef9e33b7c056c84667
Kshitij Patil <kshitijpatil98@gmail.com>
2025-04-14 17:12:38 +0530

    Add simple fn with jni binding
    Signed-off-by: Kshitij Patil <kshitijpatil98@gmail.com>

--- rustique/src/lib.rs
+++ rustique/src/lib.rs
@@ -1,7 +1,24 @@
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
+#[cfg(target_os = "android")]
+#[allow(non_snake_case)]
+pub mod android {
+    use jni::objects::JClass;
+    use jni::JNIEnv;
+    use crate::add;
 
+    // The native function implemented in Rust.
+    #[unsafe(no_mangle)]
+    pub unsafe extern "C" fn Java_com_kshitijpatil_rustique_Rustique_add(
+        _: JNIEnv,
+        _: JClass,
+        left: u64,
+        right: u64
+    ) -> u64 {
+        add(left, right)
+    }
+}
 #[cfg(test)]
 mod tests {
     use super::*;
